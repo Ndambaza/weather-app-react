3d56f4885a060ffa516a16de7f3475a42a7bccf4
Ndambaza <135227646+Ndambaza@users.noreply.github.com>
2025-11-10 18:43:18 +0000

    Added axios integration so it shows the weather of a city

---
+++ src/Weather.js
@@ -0,0 +1,26 @@
+import React from "react";
+import axios from "axios";
+import { TailSpin } from "react-loader-spinner";
+import { AnimatedWeatherIcon } from "animated-weather-icon";
+
+export default function Weather(props) {
+  function handleResponse(response) {
+    alert(
+      `The weather in ${response.data.name} is ${response.data.main.temp}°C`
+    );
+  }
+  let apiKey = `24d6cb2a7c0d27fcc186996bccf9c722`;
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+
+  axios.get(url).then(handleResponse);
+  return (
+    <div>
+      <TailSpin
+        height="80"
+        width="80"
+        color="#4fa94d"
+        ariaLabel="tail-spin-loading"
+      />
+    </div>
+  );
+}
